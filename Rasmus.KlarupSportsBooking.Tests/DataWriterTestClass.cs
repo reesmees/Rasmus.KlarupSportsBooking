c8f0c4377ae1d2e6c2f9bf2acae591fab9367f1b
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-09-17 11:47:08 +0200

    Added CreateEmailTest and CreateUnionWithExistingEmailAndAddresstest

--- Rasmus.KlarupSportsBooking.Tests/DataWriterTestClass.cs
+++ Rasmus.KlarupSportsBooking.Tests/DataWriterTestClass.cs
@@ -24,5 +24,37 @@ namespace Rasmus.KlarupSportsBooking.Tests
 
             Assert.AreEqual(addressCount + 1, handler.DB.Addresses.Count());
         }
+
+        [TestMethod]
+        public void CreateEmailTest()
+        {
+            int emailCount = handler.DB.E_mails.Count();
+            string testEmail = "testEmail";
+
+            handler.Writer.CreateEmail(testEmail);
+
+            Assert.AreEqual(emailCount + 1, handler.DB.E_mails.Count());
+        }
+
+        [TestMethod]
+        public void CreateUnionWithExistingEmailAndAddressTest()
+        {
+            int unionCount = handler.DB.Unions.Count();
+            int emailCount = handler.DB.E_mails.Count();
+            int addressCount = handler.DB.Addresses.Count();
+            string testCity = "TestCity";
+            int testZipCode = 10;
+            int testFloor = 0;
+            int testHouseNumber = 1;
+            string testStreetName = "TestStreet";
+            string testEmail = "testEmail";
+            string testName = "TestUnionName";
+
+            handler.Writer.CreateUnion(testName, testEmail, testStreetName, testHouseNumber, testFloor, testZipCode, testCity);
+
+            Assert.AreEqual(unionCount + 1, handler.DB.Unions.Count());
+            Assert.AreEqual(emailCount, handler.DB.E_mails.Count());
+            Assert.AreEqual(addressCount, handler.DB.Addresses.Count());
+        }
     }
 }
