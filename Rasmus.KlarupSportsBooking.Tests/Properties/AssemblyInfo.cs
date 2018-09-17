349c63ec1e5c81f600568d73d873e446d265412a
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-09-17 09:53:15 +0200

    Set up basic project structure, added EntityFramework to projects

---
+++ Rasmus.KlarupSportsBooking.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rasmus.KlarupSportsBooking.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rasmus.KlarupSportsBooking.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad76b236-1f9f-454a-ab82-f0742a8c79d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
