aeceee39ed1377049abeeabcc5f16655c778c49b
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-09-17 13:28:01 +0200

    Created AdminLoginWindow

---
+++ Rasmus.KlarupSportsBooking.GUI/AdminLoginWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Rasmus.KlarupSportsBooking.Business;
+using Rasmus.KlarupSportsBooking.DataAccess;
+
+namespace Rasmus.KlarupSportsBooking.GUI
+{
+    /// <summary>
+    /// Interaction logic for AdminLoginWindow.xaml
+    /// </summary>
+    public partial class AdminLoginWindow : Window
+    {
+        public DataHandler handler;
+        public AdminLoginWindow(DataHandler handler)
+        {
+            InitializeComponent();
+            this.handler = handler;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (!handler.DB.Administrators.Any(a => a.E_mails.E_mailAddress == tbxEmail.Text && a.Password == tbxPassword.Text))
+            {
+                MessageBox.Show("Forkert e-mailadresse eller kodeord");
+                tbxEmail.Text = "";
+                tbxPassword.Text = "";
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
--- Rasmus.KlarupSportsBooking.GUI/MainWindow.xaml.cs
+++ Rasmus.KlarupSportsBooking.GUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Rasmus.KlarupSportsBooking.Business;
 
 namespace Rasmus.KlarupSportsBooking.GUI
 {
@@ -20,9 +21,29 @@ namespace Rasmus.KlarupSportsBooking.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataHandler handler;
         public MainWindow()
         {
             InitializeComponent();
+            handler = new DataHandler();
+        }
+
+        private void btnOpenUnionWindow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpenAdminWindow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AdminLoginWindow adminLogin = new AdminLoginWindow(handler);
+                adminLogin.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Der skete en fejl{Environment.NewLine}{Environment.NewLine}{ex.Message}");
+            }
         }
     }
 }
