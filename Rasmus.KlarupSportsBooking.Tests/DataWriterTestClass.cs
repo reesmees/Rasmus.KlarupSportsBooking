9076484954676692b0fb3d7e644d98f65edaed60
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-09-17 11:41:49 +0200

    Added CreateAddressTest

---
+++ Rasmus.KlarupSportsBooking.Tests/DataWriterTestClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rasmus.KlarupSportsBooking.Business;
+
+namespace Rasmus.KlarupSportsBooking.Tests
+{
+    [TestClass]
+    public class DataWriterTestClass
+    {
+        public DataHandler handler = new DataHandler();
+
+        [TestMethod]
+        public void CreateAddressTest()
+        {
+            int addressCount = handler.DB.Addresses.Count();
+            string testCity = "TestCity";
+            int testZipCode = 10;
+            int testFloor = 0;
+            int testHouseNumber = 1;
+            string testStreetName = "TestStreet";
+
+            handler.Writer.CreateAddress(testStreetName, testHouseNumber, testFloor, testZipCode, testCity);
+
+            Assert.AreEqual(addressCount + 1, handler.DB.Addresses.Count());
+        }
+    }
+}
