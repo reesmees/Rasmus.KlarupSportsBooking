e8d59e74ce5f5dac6437d1681e5f0798ecdcaa19
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-09-18 11:40:22 +0200

    Added data validation to properties in UnionLeader

--- Rasmus.KlarupSportsBooking.DataAccess/UnionLeader.cs
+++ Rasmus.KlarupSportsBooking.DataAccess/UnionLeader.cs
@@ -14,11 +14,55 @@ namespace Rasmus.KlarupSportsBooking.DataAccess
 
         [Required]
         [StringLength(100)]
-        public string Name { get; set; }
+        public string Name
+        {
+            get { return Name; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Name må ikke være null");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name må ikke være udelukkende whitespace");
+                }
+                else if (value.Length > 100)
+                {
+                    throw new ArgumentException("Name må ikke være længere end 100 karakterer");
+                }
+                else
+                {
+                    Name = value;
+                }
+            }
+        }
 
         [Required]
         [StringLength(20)]
-        public string PhoneNumber { get; set; }
+        public string PhoneNumber
+        {
+            get { return PhoneNumber; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("PhoneNumber må ikke være null");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("PhoneNumber må ikke være udelukkende whitespace");
+                }
+                else if (value.Length > 20)
+                {
+                    throw new ArgumentException("PhoneNumber må ikke være længere end 20 karakterer");
+                }
+                else
+                {
+                    PhoneNumber = value;
+                }
+            }
+        }
 
         public virtual Union Union { get; set; }
     }
