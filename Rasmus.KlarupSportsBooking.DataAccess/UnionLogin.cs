a9815bce9ab5e7d68ea9efd797e0e8a0349fa9f7
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-09-18 11:41:30 +0200

    Added data validation to properties in UnionLogin

--- Rasmus.KlarupSportsBooking.DataAccess/UnionLogin.cs
+++ Rasmus.KlarupSportsBooking.DataAccess/UnionLogin.cs
@@ -14,11 +14,55 @@ namespace Rasmus.KlarupSportsBooking.DataAccess
 
         [Required]
         [StringLength(50)]
-        public string Username { get; set; }
+        public string Username
+        {
+            get { return Username; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Username må ikke være null");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Username må ikke være udelukkende whitespace");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new ArgumentException("Username må ikke være længere end 50 karakterer");
+                }
+                else
+                {
+                    Username = value;
+                }
+            }
+        }
 
         [Required]
         [StringLength(50)]
-        public string Password { get; set; }
+        public string Password
+        {
+            get { return Password; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Password må ikke være null");
+                }
+                else if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Password må ikke være udelukkende whitespace");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new ArgumentException("Password må ikke være længere end 50 karakterer");
+                }
+                else
+                {
+                    Password = value;
+                }
+            }
+        }
 
         public virtual Union Union { get; set; }
     }
